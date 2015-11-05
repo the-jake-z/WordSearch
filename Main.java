03a5d256d432dc6334967595a5913709c9da6ec0
Jacob Zarobsky <jzarob@gmail.com>
2015-11-05 10:46:08 -0600

    Added main entry point for the program

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * 		Project: 	Word Search (CS 360 Fall 2015, Project 3)
+ * 		File:		Main.java
+ * 		Author:		Jacob A. Zarobsky
+ * 		Date:		Oct 5, 2015
+ *
+ * 		This file is the main entry point for the program.
+ *      The program reads in a file and then finds the words
+ *      hidden inside of that file.
+ */
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+}
