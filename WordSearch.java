09cf87c763fbba2bd937419faa557c88fb5530c3
Jacob Zarobsky <jzarob@gmail.com>
2015-11-05 13:05:53 -0600

    added a word search class

---
+++ WordSearch.java
@@ -0,0 +1,65 @@
+/*
+ * 		Project: 	Word Search (CS 360 Fall 2015, Project 3)
+ * 		File:		WordSearch.java
+ * 		Author:		Jacob A. Zarobsky
+ * 		Date:		Nov 5, 2015
+ *
+ * 		This file runs the WordSearch and stores all
+ *      necessary data for the search.
+ */
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class WordSearch
+{
+    private String puzzleSource;
+    private String wordSource;
+
+    public void setPuzzleSource(String pSource) { puzzleSource = pSource; }
+    public String getPuzzleSource() { return puzzleSource;}
+
+    public void setWordSource(String wSource) { wordSource = wSource; }
+    public String getWordSource() { return wordSource; }
+
+    public WordSearch(String puzzleSource, String wordSource)
+    {
+        setPuzzleSource(puzzleSource);
+        setWordSource(wordSource);
+    }
+
+    public void run()
+    {
+        initalizeSources();
+    }
+
+    private void initalizeSources()
+    {
+        final FileReader fileReader = new FileReader(getPuzzleSource());
+        final String delimiters = " ";
+        fileReader.forEachLine((String line, Integer lineNumber) ->
+        {
+            if(lineNumber == 1)
+            {
+                // Do something with the count.
+            }
+            else
+            {
+                StringTokenizer tokenizer =
+                    new StringTokenizer(line, delimiters);
+
+                while(tokenizer.hasMoreTokens())
+                {
+                    System.out.println(tokenizer.nextToken());
+                }
+            }
+        });
+
+        fileReader.setFilePath(getWordSource());
+
+        fileReader.forEachLine((String line, Integer lineNumber) ->
+        {
+            System.out.println(line);
+        });
+    }
+}
