fb6fc5a509e40b104bf6a4ecd3c71e4ab6f1654f
Jacob Zarobsky <jzarob@gmail.com>
2015-11-06 19:30:33 -0600

    Finished implementation of edge class

--- Edge.java
+++ Edge.java
@@ -4,11 +4,29 @@
  * 		Author:		Jacob A. Zarobsky
  * 		Date:		Nov 6, 2015
  *
- * 		This file runs the WordSearch and stores all
- *      necessary data for the search.
+ * 		This class stores all the data associated with an edge.
  */
 
  public class Edge
  {
- 
+     // The direction to get to this vertex from whoever
+     // initalized us.
+     private Direction direction;
+
+     // The vertex this edge takes us to.
+     private Vertex toVertex;
+
+     // Property Accessors
+     public void setDirection(Direction d) { direction = d; }
+     public Direction getDirection() { return direction; }
+
+     public void setToVertex(Vertex v) { toVertex = v; }
+     public Vertex getToVertex() { return toVertex; }
+
+     // Constructor
+     public Edge(Vertex toVertex, Direction d )
+     {
+         setToVertex(toVertex);
+         setDirection(d);
+     }
  }
