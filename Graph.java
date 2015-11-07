3c193db9ecdd68baec1914b64059b5cbfda1c179
Jacob Zarobsky <jzarob@gmail.com>
2015-11-06 19:38:43 -0600

    Added graph class

---
+++ Graph.java
@@ -0,0 +1,14 @@
+/*
+ * 		Project: 	Word Search (CS 360 Fall 2015, Project 3)
+ * 		File:		Graph.java
+ * 		Author:		Jacob A. Zarobsky
+ * 		Date:		Nov 5, 2015
+ *
+ * 		This file stores all the data for the graph used to solve the
+ *      puzzle. It also contains methods to traverse the graph.
+ */
+
+public class Graph
+{
+
+}
