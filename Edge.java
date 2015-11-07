b52c4381ff08839c09cf77070a582a53c6ab383d
Jacob Zarobsky <jzarob@gmail.com>
2015-11-06 19:23:25 -0600

    added edge class

---
+++ Edge.java
@@ -0,0 +1,14 @@
+/*
+ * 		Project: 	Word Search (CS 360 Fall 2015, Project 3)
+ * 		File:		Edge.java
+ * 		Author:		Jacob A. Zarobsky
+ * 		Date:		Nov 6, 2015
+ *
+ * 		This file runs the WordSearch and stores all
+ *      necessary data for the search.
+ */
+
+ public class Edge
+ {
+ 
+ }
