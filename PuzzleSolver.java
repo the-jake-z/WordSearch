1803b38c65cba469fdf005bcc6fd2986fd6cb0e9
Jacob Zarobsky <jzarob@gmail.com>
2015-11-06 23:52:12 -0600

    Added puzzle solver class

---
+++ PuzzleSolver.java
@@ -0,0 +1,15 @@
+/*
+ * 		Project: 	Word Search (CS 360 Fall 2015, Project 3)
+ * 		File:		PuzzleSolver.java
+ * 		Author:		Jacob A. Zarobsky
+ * 		Date:		Nov 5, 2015
+ *
+ * 		This file is the main entry point for the program.
+ *      The program reads in a file and then finds the words
+ *      hidden inside of that file.
+ */
+
+ public class PuzzleSolver
+ {
+ 
+ }
